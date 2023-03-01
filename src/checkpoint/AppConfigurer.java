ad1763e4e069d5cb7cb83d325d335133d6bd35a7
Lee dong yeol <dlehdduf09020@naver.com>
2023-03-02 01:04:34 +0900

    Initial commit

---
+++ src/checkpoint/AppConfigurer.java
@@ -0,0 +1,11 @@
+package checkpoint;
+import checkpoint.discount.BEDiscountCondition;
+import checkpoint.discount.CozDiscountCondition;
+import checkpoint.discount.DiscountCondition;
+
+public class AppConfigurer {
+
+    public DiscountCondition discountCondition() {
+        return new BEDiscountCondition(20);
+    }
+}
